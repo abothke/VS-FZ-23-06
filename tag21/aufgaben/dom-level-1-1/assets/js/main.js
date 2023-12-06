0532ac16cd63beac8610569c1c5979801492b5ba
abothke <andrebothke@gmail.com>
2023-12-06 16:11:53 +0100

    aufgaben fertig

---
+++ tag21/aufgaben/dom-level-1-1/assets/js/main.js
@@ -0,0 +1,7 @@
+document.write("<h1>Hello World!</h1>");
+document.write("<p>Have a nice day!</p>");
+let myText = "Hello again"
+document.write(myText)
+let firstName = "Andre"
+let lastName = "Bothke"
+document.write("<p>Hi, ich bin " + firstName + " " + lastName + " </p>")
---
+++ tag21/aufgaben/dom-level-1-2/assets/js/main.js
@@ -0,0 +1,3 @@
+document.getElementById("info").innerHTML = "<h1>Hello World</h1> <h2>How are you?</h2>";
+document.getElementById("container").innerHTML = "<p>start of the element</p>";
+document.write("<p>end of the element</p>")
