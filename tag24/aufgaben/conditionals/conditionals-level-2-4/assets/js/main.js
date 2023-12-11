d78242b04a1cf22523428d924bdd62151ef8ad11
abothke <andrebothke@gmail.com>
2023-12-11 16:03:57 +0100

    Conditionals 2-4 fertig

---
+++ tag24/aufgaben/conditionals/conditionals-level-2-4/assets/js/main.js
@@ -0,0 +1,12 @@
+function math(num1, num2){
+    let sum = num1 + num2;
+    if (num1 == num2) sum *= 5;
+    console.log(sum); 
+}
+
+math(1, 5);
+math(5, 5);
+math(20, 30);
+math(30, 30);
+math(20, 20);
+math(21, 19);
