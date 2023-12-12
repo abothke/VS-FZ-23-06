661541c77b202be89a3116c1f1ae44549e7fc6f3
abothke <andrebothke@gmail.com>
2023-12-12 17:57:48 +0100

    Einfuehrung 2-1 done

---
+++ tag25/aufgaben/js-einfuehrung-2-1/assets/js/main.js
@@ -0,0 +1,17 @@
+function sendText() {
+  let textInput = document.querySelector("#textInput").value;
+  let alertMsg = "Gib bitte eine Nachricht ein!";
+  let emptyCheck = textInput.length;
+  if (emptyCheck == 0) {
+    document.querySelector("span").innerText = alertMsg;
+    document.querySelector("p").innerHTML = "";
+  } else {
+    document.querySelector("span").innerText = "";
+  }
+
+  if (textInput == "") {
+  } else {
+    console.log(textInput);
+    document.querySelector("p").innerHTML = textInput;
+  }
+}
