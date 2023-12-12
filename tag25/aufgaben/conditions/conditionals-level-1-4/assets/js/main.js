2d8f58fe4774a471b87edb4d063ce9101d3bb588
abothke <andrebothke@gmail.com>
2023-12-12 16:46:48 +0100

    conditionals-1-4 fertig

---
+++ tag25/aufgaben/conditions/conditionals-level-1-4/assets/js/main.js
@@ -0,0 +1,9 @@
+function checkPass() {
+  let passLength = document.querySelector("#password").value.length;
+  console.log(passLength);
+  let pwSecure =
+    passLength > 8
+      ? "Das Passwort ist sicher."
+      : "Das Passwort muss mindestens 8 Zeichen lang sein.";
+  document.querySelector("h2").innerHTML = `<h2>${pwSecure}</h2>`;
+}
