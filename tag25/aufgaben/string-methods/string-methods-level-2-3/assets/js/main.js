3d028b6105baa05822736d2fa3d51d30f89184cd
abothke <andrebothke@gmail.com>
2023-12-12 16:32:26 +0100

    string fertig, funktion zum suchen korrigiert

--- tag25/aufgaben/string-methods/string-methods-level-2-3/assets/js/main.js
+++ tag25/aufgaben/string-methods/string-methods-level-2-3/assets/js/main.js
@@ -1,4 +1,10 @@
 function highlightText() {
   let hWord = document.querySelector("#search-input").value;
+  let aText = document.querySelector("article").innerHTML;
+  aText = aText.replaceAll("<span>", "");
+  aText = aText.replaceAll("</span>", "");
+  console.log(aText);
   console.log(hWord);
+  highlight = aText.replaceAll(hWord, `<span>${hWord}</span>`);
+  document.querySelector("article").innerHTML = highlight;
 }
