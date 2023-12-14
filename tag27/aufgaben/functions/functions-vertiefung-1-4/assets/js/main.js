4cf7d219322caa35cd93e08629108fd4e3a1abfd
abothke <andrebothke@gmail.com>
2023-12-14 12:04:12 +0100

    Aufgaben Fertig

---
+++ tag27/aufgaben/functions/functions-vertiefung-1-4/assets/js/main.js
@@ -0,0 +1,11 @@
+function hero(heroName, heroPower, heroEnemy) {
+  console.log(`Mein Lieblingsheld ist: ${heroName}`);
+  console.log(`Er/Sie hat die Fähigkeit: ${heroPower}`);
+  console.log(`Sein/Ihr größter Gegner ist: ${heroEnemy}`);
+}
+
+hero(
+  "Iron Man",
+  "Seine Rüstung ist mit modernster Technik ausgestattet wie zum Beispiel Laserstrahlen",
+  "Mandarin"
+);
