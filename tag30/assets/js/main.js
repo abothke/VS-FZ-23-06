ec16dbea76d7e71751dd1cd74a247bf1aa25de78
abothke <andrebothke@gmail.com>
2023-12-19 10:44:23 +0100

    pause

--- tag30/assets/js/main.js
+++ tag30/assets/js/main.js
@@ -22,3 +22,43 @@ let numbers2 = [1, 2, 3, 24, 32];
 numbers2.reverse();
 console.log(numbers2);
 
+// map - Erstellt ein neues Array aus den Ergebnissen einer Funktion, die auf jedes Element des Arrays angewendet wird.
+
+let myNumArray = [1, 2, 3, 4, 5];
+
+let multi = myNumArray.map((num) => num * num);
+console.log(multi);
+
+// foreach - Führt eine Funktion für jedes Element eines Arrays aus.
+
+let tiere = ["Hund", "Katze", "Maus", "Pferd", "Kuh"];
+tiere.forEach((tier) => {
+  console.log(tier);
+});
+
+// Beim ersten Durchlauf ist tier = "Hund" und index = 0
+// Beim zweiten Durchlauf ist tier = "Katze" und index = 1
+// Beim dritten Durchlauf ist tier = "Maus" und index = 2
+// Beim vierten Durchlauf ist tier = "Pferd" und index = 3
+// Beim fünften Durchlauf ist tier = "Kuh" und index = 4
+
+let num = [1, 2, 3, 4, 5];
+
+num.forEach((element, index, array) => {
+  console.log("index: " + index);
+  console.log("array: " + array);
+  console.log("element: " + element);
+});
+
+// map & foreach
+
+let num2 = [1, 2, 3, 4, 5];
+
+const mapForEach = (num2) => {
+  let double = num2.map((num) => num * 2);
+  double.forEach((doubleNum) => {
+    console.log("verdoppelte Zahl: " + doubleNum);
+  });
+};
+
+mapForEach(num2);
