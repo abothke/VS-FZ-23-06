82923c47c8b5faa79be73a33a2a21d7f80551006
abothke <andrebothke@gmail.com>
2024-01-02 13:24:54 +0100

    obj 1-2 fertig

---
+++ tag34/aufgaben/objekte-level-1/objekte-level-1-2/assets/js/main.js
@@ -0,0 +1,9 @@
+const person = {
+    name: "Andre",
+    age: "34",
+    sagNameAlter: () =>{
+        alert(`Hallo, mein Name ist ${person.name} und ich bin ${person.age} jahre alt`)
+    }
+}
+
+person.sagNameAlter();
