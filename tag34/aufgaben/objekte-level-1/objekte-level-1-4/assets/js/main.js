62156ef56fba6f9b90d79306a09819ab0eb387d3
abothke <andrebothke@gmail.com>
2024-01-02 13:39:55 +0100

    obj 1-4 fertig

---
+++ tag34/aufgaben/objekte-level-1/objekte-level-1-4/assets/js/main.js
@@ -0,0 +1,26 @@
+let unsereHaustiere = [
+    {
+        tiertyp: "Katze",
+        names: [
+            "Gipsy",
+            "Nala",
+            "Dinky"
+        ]
+    },
+    {
+        tiertyp: "Hunde",
+        names: [
+            "Knöpfchen",
+            "Pinselchen",
+            "Droopy"
+        ]
+    }
+];
+
+console.log(unsereHaustiere[0].names[1]);
+console.log(unsereHaustiere[1].names[2]);
+console.log(unsereHaustiere[1].names);
+
+delete unsereHaustiere[1].names;
+unsereHaustiere[1].names = [ "Balu", "Snoopy" ]
+console.log(unsereHaustiere[1].names);
