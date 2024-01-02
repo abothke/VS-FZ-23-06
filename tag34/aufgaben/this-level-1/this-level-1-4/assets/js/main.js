dec752901d0979c59c88af9c15c4224d974f376c
abothke <andrebothke@gmail.com>
2024-01-02 16:27:38 +0100

    this 1-4 done

---
+++ tag34/aufgaben/this-level-1/this-level-1-4/assets/js/main.js
@@ -0,0 +1,12 @@
+function fahrrad() {
+    console.log(this.name);
+}
+
+let name = "Canyon";
+let obj1 = { name: "Merida", fahrrad: fahrrad };
+let obj2 = { name: "Scott", fahrrad: fahrrad };
+
+
+this.fahrrad(); // leer
+obj1.fahrrad(); // Merida
+obj2.fahrrad(); // Scott
