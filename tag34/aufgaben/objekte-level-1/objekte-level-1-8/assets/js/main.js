01dea8b2e46629810b7006552152cacea6ccd871
abothke <andrebothke@gmail.com>
2024-01-02 14:24:36 +0100

    obj 1-8 done

---
+++ tag34/aufgaben/objekte-level-1/objekte-level-1-8/assets/js/main.js
@@ -0,0 +1,37 @@
+let studentData = [
+    {
+        name: "Alex",
+        age: 23,
+        coop: false,
+        address: {
+            street: "Don Valley Business Park",
+            city: "Toronto",
+            postalCode: "ONM3C3E5"
+        },
+        emails: ["alex69@gmail.com", "alex123@yahoo.com"]
+    },
+    {
+        name: "Sandra",
+        age: 22,
+        coop: true,
+        address: {
+            street: "34 Lawrence Ave",
+            city: "Toronto",
+            postalCode: "ONM3C0E5"
+        },
+        emails: ["sandra@gmail.com", "sandra@yahoo.com"]
+    }
+];
+
+console.log(studentData);
+const studentOutput = (array) => {
+array.forEach((arrayData) => {
+console.log(arrayData.name)
+console.log(arrayData.coop)
+console.log(arrayData.emails);
+console.log(arrayData.address.city);
+}
+);
+}
+
+studentOutput(studentData)
