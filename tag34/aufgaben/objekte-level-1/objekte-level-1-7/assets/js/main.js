c2ecdd29c9d7b2390d37b2a54d1e475840a5bd06
abothke <andrebothke@gmail.com>
2024-01-02 14:10:12 +0100

    obj 1-7 plus bonus done

---
+++ tag34/aufgaben/objekte-level-1/objekte-level-1-7/assets/js/main.js
@@ -0,0 +1,38 @@
+let myMusic = [
+    {
+        artist: "The Beatles",
+        title: "Abbey Road",
+        release_year: 1969,
+        medium: ["LP", "CD", "MC", "Download"],
+        gold: true
+    },
+    {
+        artist: "Pink Floyd",
+        title: "Dark Side of the Moon",
+        release_year: 1978,
+        medium: ["CS", "CD", "LP", "Download"],
+        gold: true
+    },
+    {
+        artist: "Led Zeppelin",
+        title: "Led Zeppelin IV",
+        release_year: 1971,
+        medium: ["CS", "LP", "Download"],
+        gold: true
+    },
+    {
+        artist: "Metallica",
+        title: "Kill ’Em All und Ride the Lightning",
+        release_year: 1983,
+        medium: ["LP", "CD", "MC", "Download"],
+        gold: true
+    }
+];
+myMusic.forEach((music) => {
+if (music.release_year > 1975) {
+document.write(`${music.artist} <br/> ${music.title} <br /> ${music.medium} <br /> <br />`)
+} else {
+    document.write(`${music.artist} <br/> ${music.title} <br /> ${music.medium} <br /> ${music.release_year} <br /> <br />`)
+}
+}
+ );
